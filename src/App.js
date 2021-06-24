c5800bab70bfd8f4497bd337a3b7c3938eb07723
aditya patel <patel.aditya2014@gmail.com>
2021-06-24 13:00:45 +0530

    font selected and added to the app.css file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
